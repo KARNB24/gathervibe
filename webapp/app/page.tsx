5022b6cc172b1ce31956b5f129ec69a22fd5b2c6
KARNB24 <bhavkarn@sheridancollege.ca>
2024-03-01 18:10:23 -0500

    Setup project as Next.js 14
    - Installed Shadcn UI
    - Installed Tailwind CSS

---
+++ webapp/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="destructive" className="px-10">Hello World</Button>
+    </main>
+  );
+}
