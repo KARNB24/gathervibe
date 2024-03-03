3739aaef3b2010b686664539b8dd0fab7ddaa061
KARNB24 <bhavkarn@sheridancollege.ca>
2024-03-02 22:35:39 -0500

    Created fully functional navbar for both mobile and desktop
    - Navbar has a fully functional auth by clerk

--- webapp/app/(root)/page.tsx
+++ webapp/app/(root)/page.tsx
@@ -4,7 +4,8 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main>
-      <Button variant="destructive" className="px-10">Hello World</Button>
+      
+      
     </main>
   );
 }
