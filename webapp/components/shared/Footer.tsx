780b7dafaaa2c9562b0961e127051165679b32db
KARNB24 <bhavkarn@sheridancollege.ca>
2024-03-03 22:48:47 -0500

    Finished Home page front-end
    - Added Hero section
    - Added Footer

--- webapp/components/shared/Footer.tsx
+++ webapp/components/shared/Footer.tsx
@@ -1,6 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/Logo.png"
+            alt="logo"
+            width={150}
+            height={60}
+          />
+        </Link>
+
+        <p>&copy; 2024 GatherVibe All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
